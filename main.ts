03854eebd21ded98ad617cb7725ed4467866c67d
Anatoly03 <anatoly.wein@gmail.com>
2022-07-15 17:00:49 +0200

    add: circle render

--- main.ts
+++ main.ts
@@ -1,18 +1,37 @@
-import { Components } from './src/index'
-import { defineQuery } from 'bitecs'
+import { createEngine, Components } from './src/index'
+import { createWorld, addEntity, addComponent, defineQuery } from 'bitecs'
 
-// FPS
 let lastCalledTime = Date.now()
 let fps = 0
-
-// CANVAS
 let canvas = <HTMLCanvasElement>document.getElementById('canvas')
 let ctx = <CanvasRenderingContext2D>{}
+
+let world = createWorld()
 
 function init() {
     canvas = <HTMLCanvasElement>document.getElementById('canvas')
     ctx = canvas.getContext("2d")
     ctx.imageSmoothingEnabled = false
+    createEngine(world)
+}
+
+function createBodies() {
+    const floor = addEntity(world)
+    addComponent(world, Components.Pos, floor)
+    addComponent(world, Components.Rectangle, floor)
+
+    Components.Pos.x[floor] = canvas.width * .5
+    Components.Pos.y[floor] = 500
+    Components.Rectangle.w[floor] = canvas.width - 200
+    Components.Rectangle.h[floor] = 50
+
+    const ball = addEntity(world)
+    addComponent(world, Components.Pos, ball)
+    addComponent(world, Components.Circle, ball)
+
+    Components.Pos.x[ball] = canvas.width * .5
+    Components.Pos.y[ball] = 300
+    Components.Circle.r[ball] = 50
 }
 
 function update() {
@@ -46,11 +65,49 @@ function set_background(color: string) {
 }
 
 const rectangles_query = defineQuery([Components.Pos, Components.Rectangle])
+const circles_query = defineQuery([Components.Pos, Components.Circle])
 function render_objects() {
-    //
+    const rectangles = rectangles_query(world)
+    const circles = circles_query(world)
+
+    for (let i = 0; i < rectangles.length; i++) {
+        const eid = rectangles[i]
+        const x = Components.Pos.x[eid]
+        const y = Components.Pos.y[eid]
+        const w = Components.Rectangle.w[eid]
+        const h = Components.Rectangle.h[eid]
+
+        ctx.translate(x, y)
+        ctx.rotate(Components.Rotation.angle[eid])
+
+        ctx.strokeStyle = 'white'
+        ctx.strokeRect(- w * .5, - h * .5, w, h)
+
+        ctx.rotate(-Components.Rotation.angle[eid])
+        ctx.translate(-x, -y)
+    }
+
+    for (let i = 0; i < circles.length; i++) {
+        const eid = circles[i]
+        const x = Components.Pos.x[eid]
+        const y = Components.Pos.y[eid]
+        const r = Components.Circle.r[eid]
+
+        ctx.translate(x, y)
+        ctx.rotate(Components.Rotation.angle[eid])
+
+        ctx.strokeStyle = 'white'
+        ctx.beginPath()
+        ctx.ellipse(0, 0, r, r, Math.PI / 4, 0, 2 * Math.PI)
+        ctx.stroke()
+
+        ctx.rotate(-Components.Rotation.angle[eid])
+        ctx.translate(-x, -y)
+    }
 }
 
 window.onload = () => {
     init()
     update()
+    createBodies()
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import { IWorld } from 'bitecs'
 import * as Components from './comps'
+
+export function createEngine (world: IWorld): IWorld {
+
+
+    return world
+}
 
 export {
     Components
