906e8622536d64a90bc2ffc0e7cffb066f66ef84
Anatoly03 <anatoly.wein@gmail.com>
2022-07-15 16:26:42 +0200

    change: rotation attributes

--- src/comps.ts
+++ src/comps.ts
@@ -6,48 +6,33 @@ import { defineComponent, Types } from 'bitecs'
 //
 //
 
-/**
- * @component **Entity Position**
- * @description This will give the entity a position
- */
+
 export const Pos = defineComponent({
     x: Types.f64,
     y: Types.f64
 })
 
-/**
- * @component **Entity Velocity**
- * @description This will give the entity the ability to move. Entities without
- * Velocity are static.
- */
 export const Vel = defineComponent({
     x: Types.f64,
     y: Types.f64
 })
 
-/**
- * @component **Entity Acceleration**
- * @description This will give the entity the ability to change velocity.
- */
 export const Acc = defineComponent({
     x: Types.f64,
     y: Types.f64
 })
 
-/**
- * @component **Entity Rotation**
- * @description An entity will rotate if this component is present.
- */
 export const Rotation = defineComponent({
-    x: Types.f64,
-    y: Types.f64
+    angle: Types.f64,
 })
 
 //
 //
-// OBJECTS
+// BODIES / OBJECTS
 //
 //
+
+export const Body = defineComponent()
 
 export const Rectangle = defineComponent({
     w: Types.f64,
